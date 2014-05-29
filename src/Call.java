1e005caedb6dfcf9ef8aac4506808a1142b378f0
martj <martj@mail.bg>
2014-05-29 12:20:08 +0300

    few of the tasks

--- src/Call.java
+++ src/Call.java
@@ -1,7 +1,48 @@
-
 public class Call {
+    private int data;
+    private int startTime;
+    private int callTime;
+    private Call[] callHistory = new Call[16];
+    private int callCounter;
+    public int getData() {
+        return data;
+    }
 
-	//data 
-//vreme na zapochvane na razgovora
-//produljitelnost na razgovora
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getCallTime() {
+        return callTime;
+    }
+
+    public void setCallTime(int callTime) {
+        this.callTime = callTime;
+    }
+
+    public Call[] getCallHistory() {
+        return callHistory;
+    }
+
+    public void setCallHistory(Call[] callHistory) {
+        this.callHistory = callHistory;
+    }
+
+    public int getCallCounter() {
+        return callCounter;
+    }
+
+    public void setCallCounter(int callCounter) {
+        this.callCounter = callCounter;
+    }
+
+    
 }
